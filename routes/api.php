708380c0f281fa89b489ae6a6cfeff038008bccb
kasih susilawati <kasihinces@gmail.com>
2023-02-03 19:25:52 +0700

    first commit

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiswaC;
+
+Route::get('/', function(){
+      dd('Hello World !');
+});
+
+Route::get('/siswa', [SiswaC::class, 'index']);
