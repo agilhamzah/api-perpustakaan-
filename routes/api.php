bd15acb6b35b51d2dec5dc0b7e7aa3c49b7271c2
kasih susilawati <kasihinces@gmail.com>
2023-02-15 11:39:36 +0700

    first commit

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SiswaC;
+use App\Http\Controllers\PostC;
 
 Route::get('/', function(){
-      dd('Hello World !');
+      return 'Hello World !';
 });
 
-Route::get('/siswa', [SiswaC::class, 'index']);
-Route::post('/siswa/store', [perpustakaan::class, 'store']);
-Route::get('/siswa/show/{id}', [perpustakaan::class, 'show']);
-Route::post('/siswa/update/{id}', [perpustakaan::class, 'update']);
-Route::get('/siswa/destroy/{id}', [perpustakaan::class, 'destroy']);
+Route::get('/posts', [Postc::class, 'index']);
