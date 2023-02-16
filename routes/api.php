8aceb9976c1a7c83f174e65ca4ddb2204c2ddd46
Agilmhamzah <agilmhamzah4@gmail.com>
2023-02-16 15:03:58 +0700

    frist commit

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,20 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthC;
 use App\Http\Controllers\PostC;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
       return 'Hello World !';
 });
 
-Route::get('/posts', [Postc::class, 'index']);
-Route::get('/posts/{id}', [Postc::class, 'detail']);
+Route::get('/posts', [Postc::class, 'index'])->middleware(['auth:sanctum']);
+Route::get('/posts/{id}', [Postc::class, 'detail'])->middleware(['auth:sanctum']);
 
+Route::post('/login', [AuthC::class, 'login']);
+
+Route::get('/password', function(){
+      return Hash::make('rahasia');
+});
